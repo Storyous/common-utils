5e06564d2ffadeed674e6022687f752f3142d20b
Stanley Gurnik <sgurnik@gmail.com>
2018-06-05 15:58:14 +0200

    Renamed appError => AppError + bump version + add common help string to prometheus client

---
+++ default.config.js
@@ -0,0 +1,27 @@
+'use strict';
+
+// Config structure that must be preserved
+
+module.exports = {
+    logging: {
+        console: {
+            silent: false,
+            level: 'info',
+            colorize: true
+        },
+        graylog: {
+            silent: true,
+            server: {
+                adapterOptions: {
+                    host: 'graylog2.storyous.xyz',
+                    port: 12216
+                }
+            }
+        },
+        sentry: {
+            dsn: process.env.SENTRY_DSN || '',
+            silent: true,
+            level: 'error'
+        }
+    }
+};
