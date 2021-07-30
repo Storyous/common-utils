93d58bb7f17b278e3f20161eece8ddee6467ff0b
Michal Timko <timko.michal@centrum.cz>
2021-07-30 14:56:12 +0200

    encode functionality rewriten

--- src/models/permissions/permissionHelper.js
+++ src/models/permissions/permissionHelper.js
@@ -1,173 +1,35 @@
+
 const joi = require('joi');
-
-const { InvalidInputError } = require('./permissionError');// eslint-disable-line import/order
-/**
- * @typedef {
- *  Array<string>
- * } ParsedString
- *
- */
-
-/**
- * @typedef {{
- *   b:string,
- *   h:string
- * }[]} ConversionTable
- *
- */
-
-const conversionTable = [
-    { b: '0000', h: '0' },
-    { b: '0001', h: '1' },
-    { b: '0010', h: '2' },
-    { b: '0011', h: '3' },
-    { b: '0100', h: '4' },
-    { b: '0101', h: '5' },
-    { b: '0110', h: '6' },
-    { b: '0111', h: '7' },
-    { b: '1000', h: '8' },
-    { b: '1001', h: '9' },
-    { b: '1010', h: 'A' },
-    { b: '1011', h: 'B' },
-    { b: '1100', h: 'C' },
-    { b: '1101', h: 'D' },
-    { b: '1110', h: 'E' },
-    { b: '1111', h: 'F' },
-];
+const _ = require('lodash');
+const { InvalidInputError } = require('./permissionError'); // eslint-disable-line import/order
 
 /**
  *
- * @param {string} str
- * @param {number} size
- * @returns {ParsedString}
- */
-
-function chunkSubstr (str, size) {
-    const numChunks = Math.ceil(str.length / size);
-    const chunks = new Array(numChunks);
-
-    for (let i = 0, o = 0; i < numChunks; ++i, o += size) {
-        chunks[i] = str.substr(o, size);
-    }
-
-    return chunks;
-}
-
-/**
- *
- * @param {string} data
- * @param {ConversionTable} table
- * @returns {string}
- */
-
-function binaryToHex (data, table) {
-    return table.find((i) => i.b === data)?.h || '0';
-
-}
-
-/**
- *
- * @param {string} data
- * @param {ConversionTable} table
- * @returns {string}
- */
-
-function hexToBinary (data, table) {
-    return table.find((i) => i.h === data)?.b;
-}
-
-/**
- *
- * @param {ParsedString} data
- * @param {ConversionTable} table
- * @returns {string}
- */
-
-function convertArrayToHex (data, table) {
-    let result = '';
-    data.forEach((i) => {
-        result += binaryToHex(i, table);
-    });
-    return result;
-}
-
-/**
- *
- * @param {ParsedString} data
- * @param {ConversionTable} table
- * @returns {string}
- */
-
-function convertArrayToBinary (data, table) {
-    let result = '';
-    data.forEach((i) => {
-        result += hexToBinary(i, table);
-    });
-    return result;
-}
-/**
- *
- * @param {string} input
- * @returns {string|function}
- */
-
-function completeString (input) {
-    return input.length % 4 === 0 ? input : completeString(input.concat('0'));
-}
-
-/**
- *
- * @param {string} data
- * @returns {ParsedString}
- */
-function prepareData (data) {
-    return chunkSubstr(completeString(data), 4);
-}
-
-/**
- *
- * @param {boolean[]} data
- * @returns {string}
- */
-
-function convertBooleanArrayToString (data) {
-    let result = '';
-    data.forEach((i) => {
-        result += i ? '1' : '0';
-    });
-    return result;
-}
-/**
- *
- * @param {string} data
+ * @param {string} binaryString
  * @returns {boolean[]}
  */
-
-function binaryStringToBooleanArray (data) {
-    const parsedString = data.split('');
+function binaryStringToBooleanArray(binaryString) {
+    const parsedString = binaryString.split('');
     return parsedString.map((i) => (i === '1'));
-
 }
-
 /**
  *
- * @param data
+ * @param {string} hexString
  */
-function validateHexInput (data) {
-    const regExp = new RegExp('^[0-9A-F]+$');
-    if (!regExp.test(data)) throw new InvalidInputError(data);
+function validateHexInput(hexString) {
+    const regExp = new RegExp('^[0-9a-fA-F]+$');
+    if (!regExp.test(hexString))
+        throw new InvalidInputError(hexString);
 }
-
 /**
  *
- * @param data
+ * @param {boolean[]} booleanArray
  */
-
-function validateBooleanString (data) {
+function validateBooleanArray(booleanArray) {
     const schema = joi.array().items(joi.boolean());
-    const validationResponse = schema.validate(data, { convert: false });
+    const validationResponse = schema.validate(booleanArray, { convert: false });
     if (validationResponse.error) {
-        throw new InvalidInputError(data);
+        throw new InvalidInputError(booleanArray);
     }
 }
 /**
@@ -175,20 +37,35 @@ function validateBooleanString (data) {
  * @param {boolean[]} data
  * @returns {string}
  */
-function encodeData (data) {
-    validateBooleanString(data);
-    return convertArrayToHex(prepareData(convertBooleanArrayToString(data)), conversionTable);
+function encodeData(booleanArray) {
+    validateBooleanArray(booleanArray);
+    const res = _.chunk(booleanArray, 4)
+        .map(chunk => {
+            const binaryString = chunk.map(bool => bool ? '1' : '0')
+                .join('')
+                .padEnd(4, '0');
+            return parseInt(binaryString, 2)
+                .toString(16);
+        })
+        .join('');
+    return res
 }
+
 /**
  *
  * @param {string} data
  * @returns {boolean[]}
  */
+function decodeData(hexString) {
+    validateHexInput(hexString);
+    const binaryString = _.chunk(hexString, 1)
+        .map(char => {
 
-function decodeData (data) {
-    validateHexInput(data);
-    const parsedString = data.split('');
-    return binaryStringToBooleanArray(convertArrayToBinary(parsedString, conversionTable));
+            return parseInt(char, 16)
+                .toString(2).padStart(4,"0")
+        })
+        .join('');
+    return binaryStringToBooleanArray(binaryString);
 }
-const permissionHelper={encodeData, decodeData }
-module.exports = permissionHelper
+const permissionHelper = { encodeData, decodeData };
+module.exports = permissionHelper;
--- test/permissionsMockData.js
+++ test/permissionsMockData.js
@@ -1,5 +1,8 @@
-const validArrayInputs = [[true, true, true, false, false, true],
-    [false, false, false, true, true], [true, false, false, false, false]];
+const validArrayInputs = [
+    [true, true, true, false, false, true],
+    [false, false, false, true, true],
+    [true, false, false, false, false],
+    [true,false,true,false]];
 
 const invalidArrayInputs = [
     [true, true, true, 0],
@@ -7,12 +10,16 @@ const invalidArrayInputs = [
     [true, true, false, false, 'false'],
     [true, true, true, false, false, 'true']];
 
-const expectedHexResult = ['E4', '18', '80'];
+const expectedHexResult = ['e4', '18', '80',"a"];
 
-const expectedArrayResult = [[true, true, true, false, false, true, false, false],
-    [false, false, false, true, true, false, false, false], [true, false, false, false, false, false, false, false]];
+const expectedArrayResult = [
+    [true, true, true, false, false, true, false, false],
+    [false, false, false, true, true, false, false, false],
+    [true, false, false, false, false, false, false, false],
+    [true,false,true,false]
+];
 
-const invalidHexInputs = ['12W', '22a', true];
+const invalidHexInputs = ['12W', '22an', true];
 module.exports = {
     validArrayInputs, expectedHexResult, invalidArrayInputs, invalidHexInputs, expectedArrayResult,
 };
